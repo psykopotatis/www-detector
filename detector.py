a5b3255e47a7dcbd09dd6d6066789fec5d34e00d
psykopotatis <iingemar@gmail.com>
2022-04-08 19:39:33 +0200

    Add slugify file name

--- detector.py
+++ detector.py
@@ -2,24 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 import hashlib
 import pickler
+from urls import urls
+from slugify import slugify
 
 
-url = "URL-HERE"
-pickle_file = "hash.pickle"
-print("[GET] %s" % url)
-r = requests.get(url)
-print(r.status_code)
+for url in urls:
+    pickle_file = slugify(url) + ".pickle"
+    print("[GET] %s" % url)
+    r = requests.get(url)
 
-soup = BeautifulSoup(r.text, 'html.parser')
-pipeline = soup.find(id="pipeline")
+    soup = BeautifulSoup(r.text, 'html.parser')
+    pipeline = soup.find(id="pipeline")
 
-hash = hashlib.md5(pipeline.encode('utf-8')).hexdigest()
-print(hash)
-previous_hash = pickler.load(pickle_file, "asdf")
-print(previous_hash)
+    hash = hashlib.md5(pipeline.encode('utf-8')).hexdigest()
+    print(hash)
+    previous_hash = pickler.load(pickle_file, "asdf")
+    print(previous_hash)
 
-if previous_hash == hash:
-    print("Same: %s" % url)
-else:
-    print("Change detected: %s" % url)
-    pickler.dump(pickle_file, hash)
+    if previous_hash == hash:
+        print("Same!")
+    else:
+        print("Change detected!")
+        pickler.dump(pickle_file, hash)
